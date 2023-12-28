b54e648121a05d6fc1bf6415dca610604813083a
VidhiSharma <vidhiav2611@gmail.com>
2023-12-28 21:23:06 +0100

    terminado usuario y creado el enum de Genero.h

---
+++ enums/Genero.h
@@ -0,0 +1,14 @@
+//
+// Created by Vidhi on 28/12/2023.
+//
+
+#ifndef GRUPO3_ACT4_GENERO_H
+#define GRUPO3_ACT4_GENERO_H
+
+enum Genero {
+    Femenino,
+    Masculino,
+    Otros
+};
+
+#endif //GRUPO3_ACT4_GENERO_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include "Usuario.h"
+#include "Contacto.h"
+#include "ListaEnlazada.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    //std::cout << "Hello, World!" << std::endl;
+    //return 0;
+
+    ListaEnlazada<Usuario> usuarios;
+    Usuario *usuario1 = new Usuario("a", "jef", "123c", "hola", 2, Femenino);
+    usuario1->anhadirUsuario(*usuario1);
+    usuarios.insertFinal(*usuario1);
+
+    Usuario *usuario2 = new Usuario("s", "bob", "321c", "adios", 4, Masculino);
+    usuario2->anhadirUsuario(*usuario2);
+    usuarios.insertFinal(*usuario2);
+    cout << "-----------------" << endl;
+    usuario1->leerUsuario();
+    cout << "-----------------" << endl;
+    usuario2->leerUsuario();
+    cout << "-----------------" << endl;
+
+    usuario1->buscarEmail("a", usuarios);
+    cout << "-----------------" << endl;
+    usuario1->actualizarUsuario("a", Usuario("d", "jef", "123c", "holi", 2, Femenino));
+    usuario1->leerUsuario();
+    cout << "-----------------" << endl;
+    usuario2->eliminarUsuario("s");
+    usuario2->leerUsuario();
+
+
+
+
 }
