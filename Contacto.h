5fd04b680f7b63cffda8f78b76901966aa67be3d
VictorBellod <victorbellod.oficial@gmail.com>
2024-01-02 17:34:48 +0100

    Contacto creado

--- Contacto.h
+++ Contacto.h
@@ -16,32 +16,55 @@ private:
     string info;
     vector<Contacto> contactos;
 public:
+    /**
+     * Constructor a vacio de Contacto
+     */
     Contacto(){
         email = "";
         info = "";
     }
-
+    /**
+     * Constructor parametrizado de contacto
+     * @param email email del contacto
+     * @param info informacion del contacto
+     */
     Contacto(string email, string info){
         Contacto::info = info;
         Contacto::email = email;
     }
 
+    /**
+     * Metodo para ver todos los datos de un contacto
+     */
      void verInfo(){
         cout << "Email: " << email << ", Info: " << info << endl;
     }
 
+    /**
+     * Getter del email del contacto
+     * @return email
+     */
     const string &getEmail() const {
         return email;
     }
-
+    /**
+     * Setter del email del contacto
+     * @param email
+     */
     void setEmail(const string &email) {
         Contacto::email = email;
     }
-
+    /**
+     * Getter de la informacion del contacto
+     * @return info
+     */
     const string &getInfo() const {
         return info;
     }
-
+    /**
+     * Setter de la informacion del contacto
+     * @param info
+     */
     void setInfo(const string &info) {
         Contacto::info = info;
     }
@@ -49,7 +72,11 @@ public:
 };
 
 
-
+/**
+ * Funcion para poder modificar la informacion del contacto
+ * @param email
+ * @param contactos
+ */
 void modificarInfoDelContacto(string email, ListaEnlazada<Contacto> &contactos){
     bool found = false;
     for (int i = 0; i < contactos.size(); ++i) {
@@ -65,7 +92,11 @@ void modificarInfoDelContacto(string email, ListaEnlazada<Contacto> &contactos){
         cout << "No se ha encontrado" << endl;
     }
 }
-
+/**
+ * Funcion que te permite buscar tu contacto mediante el email
+ * @param email
+ * @param contactos
+ */
 void busquedaDeContacto(string email, ListaEnlazada<Contacto> &contactos){
     bool found = false;
     for (int i = 0; i < contactos.size(); ++i){
@@ -78,7 +109,11 @@ void busquedaDeContacto(string email, ListaEnlazada<Contacto> &contactos){
         cout << "No se ha encontrado" << endl;
     }
 }
-
+/**
+ * Funcion para eliminar contactor
+ * @param email
+ * @param contactos
+ */
 void eliminarContacto(string email, ListaEnlazada<Contacto> &contactos){
     for (int i = 0; i < contactos.size(); ++i){
         if (email == contactos.at(i)->getDato().getEmail()){
